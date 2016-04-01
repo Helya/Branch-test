ed228fc517d8ef65d39b8ed965e330c31e4fe4da
余敏侠 <yuminxia@yuminxiademac-mini.local>
2016-04-01 09:11:01 +0800

    第一次

---
+++ Branch/Branch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Branch
+//
+//  Created by 余敏侠 on 16/4/1.
+//  Copyright © 2016年 secrui Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Branch/Branch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Branch
+//
+//  Created by 余敏侠 on 16/4/1.
+//  Copyright © 2016年 secrui Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
